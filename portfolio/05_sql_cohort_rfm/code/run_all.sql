4abdc7e9166823e26f64e73c07750ce139367871
nazmulishan <nazmulislamishan7@gmail.com>
2026-05-18 03:38:00 +0200

    Delete portfolio directory

--- portfolio/05_sql_cohort_rfm/code/run_all.sql
+++
@@ -1,7 +0,0 @@
--- One-command pipeline. Run with:  psql -d olist -f run_all.sql
-\i 00_schema.sql
-\i 01_data_load.sql
-\i 02_unified_view.sql
-\i 03_cohort_retention.sql
-\i 04_rfm_segmentation.sql
-\i 05_customer_lifetime_value.sql
